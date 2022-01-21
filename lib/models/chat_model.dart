4f73faf83e02f0827b2f7558fb96a6fca803199e
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2022-01-21 12:44:40 +0100

    Add files via upload

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,71 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Oluwasola",
+      message: "YO, bro whats up !",
+      time: "15:30",
+      avatarUrl:
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Finnocentlazarus%2Fdope-pics%2F&psig=AOvVaw3cJygbdZAw9VjkJpgUAQUs&ust=1627498658667000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDz7o73g_ICFQAAAAAdAAAAABAD"),
+  new ChatModel(
+      name: "Obinna",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Ayobami",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Praise",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Micheal",
+      message: "Have you gotten the stuff!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Isreal",
+      message: "How far the assignment na !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Merit",
+      message: "Ode!",
+      time: "15:26",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "LMU Computer group chat ",
+      message: "Shemmie: where is it !",
+      time: "08:00",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Trojan",
+      message: "Tomorrow sha !",
+      time: "14:20",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Dominion",
+      message: "No cap guy!",
+      time: "11:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,96 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterwhatsapp/pages/call_screen.dart';
+import 'package:flutterwhatsapp/pages/camera_screen.dart';
+import 'package:flutterwhatsapp/pages/chat_screen.dart';
+import 'package:flutterwhatsapp/pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  WhatsAppHome({this.cameras});
+
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  bool showFab = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 4);
+    _tabController.addListener(() {
+      if (_tabController.index == 1) {
+        showFab = true;
+      } else {
+        showFab = false;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsChat"),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.wifi),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.phone),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.more_vert),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CameraScreen(widget.cameras),
+          ChatScreen(),
+          StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: showFab
+          ? FloatingActionButton(
+              backgroundColor: Colors.red,
+              child: Icon(
+                Icons.house,
+                color: Colors.white,
+              ),
+              onPressed: () => print("open chats"),
+            )
+          : null,
+    );
+  }
+}
